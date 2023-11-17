273a04295ab257bf3afbf9fe03c085b44bfef459
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-17 23:45:12 +0530

    updated

--- blogsite/src/main.jsx
+++ blogsite/src/main.jsx
@@ -1,11 +1,32 @@
- import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {createBrowserRouter,RouterProvider,} from 'react-router-dom'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Blog from './pages/Blog.jsx'
+import Contact from './pages/Contact.jsx'
+import Services from './pages/Services.jsx'
+
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Home/>}  />
+      <Route path='about' element={<About/>} />
+      <Route path='Blog' element={<Blog/>} />
+      <Route path='Contact' element={<Contact/>} />
+      <Route path='Services' element={<Services/>} />
+    </Route>
+  )
+)
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
