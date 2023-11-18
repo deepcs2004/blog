9dce94cc65e6d6ad9336f3f1587b07937655e54f
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-18 23:33:12 +0530

    updated

--- blogsite/src/Componentss/BlogPage.jsx
+++ blogsite/src/Componentss/BlogPage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+// import blogData from '../../api/blogData.json'
+import BlogCard from './BlogCard';
 function BlogPage() {
-  return (
-    <div>BlogPage</div>
-  )
+
+    const [blogs, setBlogs] = useState([]);
+
+    useEffect(() => {
+
+        async function fetchBlogs (){
+            let url = `https://raw.githubusercontent.com/deepcs2004/blog/main/blogsite/api/blogData.json`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+            setBlogs(data);
+            // console.log(data);
+        }
+        fetchBlogs();
+
+    }, [])
+
+    
+    return (
+        <div>
+            {/* catagory section */}
+            <div>Page Category</div>
+
+
+
+            {/* blogSection */}
+            <div>
+                <BlogCard blogs={blogs} />
+            </div>
+
+
+
+            {/* pagination */}
+            <div>pagination</div>
+
+
+        </div>
+    )
 }
 
 export default BlogPage
