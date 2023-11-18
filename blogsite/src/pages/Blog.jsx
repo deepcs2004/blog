70ad6b3d9cb4312e053c4b6dc9a6084370180e35
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-18 20:23:48 +0530

    updated

--- blogsite/src/pages/Blog.jsx
+++ blogsite/src/pages/Blog.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import BlogPage from '../Componentss/BlogPage'
 
 function Blog() {
   return (
     <div>
       <div className='py-40 bg-black text-center text-white
       px-4'>
-        <h2 className='text-3xl lg:text-5xl leading-snug font-bold mb-5'>Blog Page</h2>
+        <h2 className='text-3xl lg:text-5xl leading-snug font-bold '>Blog Page</h2>
       </div>
+
+      {/* all blog container  */}
+      <div className='max-w-7xl mx-auto'>
+        <BlogPage/>
+      </div>
+
     </div>
   )
 }
