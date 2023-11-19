d7dfdcee1aa39082c1f9b61c4af799407f1b0a84
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-19 20:48:52 +0530

    updated

--- blogsite/src/Componentss/BlogPage.jsx
+++ blogsite/src/Componentss/BlogPage.jsx
@@ -2,22 +2,24 @@ import React, { useEffect, useState } from 'react'
 // import blogData from '../../api/blogData.json'
 import BlogCard from './BlogCard';
 import Pagination from './Pagination';
+import CategorySelection from './CategorySelection';
+import SideBar from './SideBar';
 function BlogPage() {
 
     const [blogs, setBlogs] = useState([]);
-    const [currentPage, setCurrentPage]= useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
     const pagesize = 12
     const [selectedCategory, setSelectedCategory] = useState(null);
     const [activeCategory, setActiveCategory] = useState(null);
 
     useEffect(() => {
 
-        async function fetchBlogs (){
+        async function fetchBlogs() {
             let url = `https://raw.githubusercontent.com/deepcs2004/blog/main/blogsite/api/blogData.json?page=${currentPage}&limit=${pagesize}`;
 
             // filter by category 
-            if(selectedCategory){
-                url +=`&category=${selectedCategory}`;
+            if (selectedCategory) {
+                url += `&category=${selectedCategory}`;
             }
             const response = await fetch(url);
             const data = await response.json();
@@ -26,17 +28,17 @@ function BlogPage() {
         }
         fetchBlogs();
 
-    }, [pagesize,currentPage,selectedCategory])
+    }, [pagesize, currentPage, selectedCategory])
 
-    
+
     // page chnging
-    const handlePageChange =(pageNumber)=>{
+    const handlePageChange = (pageNumber) => {
         setCurrentPage(pageNumber);
     }
 
 
     // change category
-    const handleCategoryChange =(category)=>{
+    const handleCategoryChange = (category) => {
         setSelectedCategory(category);
         setCurrentPage(1);
         setActiveCategory(category);
@@ -45,20 +47,28 @@ function BlogPage() {
     return (
         <div>
             {/* catagory section */}
-            <div>Page Category</div>
+            <div>
+                <CategorySelection onSelectCategory={handleCategoryChange} selectedCategory={selectedCategory}
+                    activeCategory={activeCategory} />
+            </div>
 
 
 
             {/* blogSection */}
-            <div>
+            <div className='flex flex-col lg:flex-row gap-12'>
                 <BlogCard blogs={blogs} currentPage={currentPage} selectedCategory={selectedCategory} pagesize={pagesize} />
+
+                {/* side bar component */}
+                <div>
+                    <SideBar />
+                </div>
             </div>
 
 
 
             {/* pagination */}
             <div>
-                <Pagination onPageChnage={handlePageChange} currentPage={currentPage} blogs={blogs} pagesize={pagesize}  />
+                <Pagination onPageChnage={handlePageChange} currentPage={currentPage} blogs={blogs} pagesize={pagesize} />
             </div>
 
 
