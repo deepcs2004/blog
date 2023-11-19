56411490c4ba4f51fe8fe3cf8942ded1ed084104
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-19 21:19:47 +0530

    updated

--- blogsite/src/pages/Home.jsx
+++ blogsite/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Banner from '../Componentss/Banner'
+import BlogPage from '../Componentss/BlogPage'
 
 function Home() {
   return (
     <div>
       <Banner/>
+      <div className='mx-w-7xl mx-auto'>
+        <BlogPage/>
+      </div>
+
     </div>
   )
 }
