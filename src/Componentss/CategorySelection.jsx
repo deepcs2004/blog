3be2c920c15fa9b7edd7bd8fe65da51879e3de02
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-24 10:56:55 +0530

    updated

--- src/Componentss/CategorySelection.jsx
+++ src/Componentss/CategorySelection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function CategorySelection({ onSelectCategory, activeCategory }) {
 
-    const categories = ["Startups", "Security", "AI", "Apps", "Tech"];
+    const categories = ["Startups", "Security", "AI", "Apps", "Tech", "Offer", "Trending"];
 
 
 
