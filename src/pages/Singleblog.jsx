49f3d0cffbdd3afc8593def82e7c1e643fb42da2
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-24 18:15:33 +0530

    updated

--- src/pages/Singleblog.jsx
+++ src/pages/Singleblog.jsx
@@ -58,7 +58,7 @@ const Singleblog = () => {
                         {elementWithId.content3}
                     </h3>
 
-                    <VideoPlayer videoId={elementWithId.videoId}/>
+                    <VideoPlayer videoUrl={videoUrl}/>
 
                     <h3 className='text-xl font-bold text-gray-800 mb-6 '>
                         {elementWithId.content4}
