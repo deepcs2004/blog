ca95457c3c61309af2b1f7473aed21c4582368d2
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-24 18:30:27 +0530

    updated

--- src/pages/Singleblog.jsx
+++ src/pages/Singleblog.jsx
@@ -59,7 +59,8 @@ const Singleblog = () => {
                         {elementWithId.content3}
                     </h3>
 
-                    <VideoPlayer videoId={elementWithId.videoId}/>
+                    <iframe width="560" height="315" src={elementWithId.videoSrc} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    
 
                     <h3 className='text-xl font-bold text-gray-800 mb-6 '>
                         {elementWithId.content4}
