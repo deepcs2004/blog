75b0b4fcd6beaae2e96477672fe475362f614dfe
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-11-25 15:46:04 +0530

    updated

--- src/pages/Singleblog.jsx
+++ src/pages/Singleblog.jsx
@@ -28,7 +28,7 @@ const Singleblog = () => {
 
             {/* blog details */}
             <div className='max-w-7xl mx-auto my-12 flex flex-col md:flex-row gap-12'>
-                <div className='lg:w-3/4 mx-auto sm:p-5'>
+                <div className='lg:w-3/4 mx-auto '>
                     <div>
                         <img src={elementWithId.image} alt="img" className='w-full mx-auto rounded' />
                     </div>
