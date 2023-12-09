5a1ec7bcadec69d137a37d60290228eb08a5f658
Deep Pal <112393265+deepcs2004@users.noreply.github.com>
2023-12-09 22:00:10 +0530

    Update Banner.jsx

--- src/Componentss/Banner.jsx
+++ src/Componentss/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
             <div className='text-white text-center'>
                 <h1 className='text-3xl lg:text-5xl leading-snug font-bold mb-5'>Welcome to Our Blog</h1>
 
-                <p className='text-gray-100 text-lg mb-5'>Read our Blogs today and join a community of intellectuals! </p>
+                <p className='text-gray-100 text-lg mb-5'>Read our Blogs today and join a community of intellectuals </p>
 
                 <div>
                     <Link to="/about" className='font-medium hover:text-yellow-500 inline-flex items-center py-1'>Learn more <FaArrowRight className='mt-1 ml-2' /></Link>
